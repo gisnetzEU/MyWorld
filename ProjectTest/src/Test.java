c5ff17a6d5eb4c9c0af081a8ff168159ef29694e
Alumne_mati1 <rbeyloc@gmail.com>
2022-04-01 10:24:20 +0200

    Modify main Test

--- ProjectTest/src/Test.java
+++ ProjectTest/src/Test.java
@@ -1,7 +1,14 @@
 public class Test {
 
     public static void main(String[] args) {
+
         Person david = new Person("David", "López", 25);
+        david.sayHelloPerson();
         System.out.print(david.name + " is adult? " + david.isAdult());
+
+        Person alex = new Person("Alex", "Picanyol", 40, false);
+        alex.sayHelloPerson();
+        System.out.print(alex.name + " is adult? " + alex.isAdult());
+
     }
 }
